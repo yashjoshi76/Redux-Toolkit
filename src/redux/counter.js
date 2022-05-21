77ad579638ba2a529fb26effcbc376d8248afe13
Yash Joshi <yash.joshi76@gmail.com>
2022-05-21 21:37:25 +0100

    conditional decrement

--- src/redux/counter.js
+++ src/redux/counter.js
@@ -12,6 +12,10 @@ export const counterSlice = createSlice({
       state.count += 1;
     },
     decrement: (state) => {
+     if(state.count === 0) {
+       return;
+     }
+     
       state.count -= 1;
     },
     incrementByAmount: (state, action) => {
